701f2048a19c4c179064e9f5d1ff0badba0092c3
administrator <administrator@administrators-Mac-mini.local>
2025-10-22 01:33:14 -0700

    renamed dist to docs for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3rd-anniversary/', // Replace with your repo name
-
+  base: '/3rd-anniversary/',
+  build: {
+    outDir: 'docs', // Change output directory to docs for GitHub Pages
+  }
 })
